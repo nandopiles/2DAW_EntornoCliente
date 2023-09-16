2747c903f76c8bc3d8f82c9055d3f0d8b9b60ac5
Nando PL <nandopiless@gmail.com>
2023-09-16 11:40:14 +0200

    better modifications for ej8

--- Ej1_Pruebas/ej8.js
+++ Ej1_Pruebas/ej8.js
@@ -1,20 +1,23 @@
-let number
+let position
+let sequence
 
 /**
  ** Creates an Array with the Fibonacci's sequence since the position passed by parameter
- * @param {Number} position position where the sequence has to stop
+ * @param {Number} max position where the sequence has to stop
  * @returns {Array} Fibonacci's sequence
  */
-fibonacciSequence = (position) => {
+fibonacciSequence = (max) => {
     let sequence = [1, 1]
-    for (let i = 2; i < position; i++) {
-        let nextResults = sequence[i - 1] + sequence[i - 2]
-        sequence.push(nextResults)
+    let nextResult
+
+    for (let i = 2; i < max; i++) {
+        nextResult = sequence[i - 1] + sequence[i - 2]
+        sequence.push(nextResult)
     }
     return sequence
 }
 
-number = prompt("Enter a position:")
-let sequence = fibonacciSequence(number)
+position = prompt("Enter a position:")
+sequence = fibonacciSequence(position)
 console.log(sequence);
-alert(`Result => ${sequence[parseInt(number - 1)]}`)
+alert(`Result => ${sequence[parseInt(position - 1)]}`)
