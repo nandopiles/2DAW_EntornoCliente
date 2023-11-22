7f5afd7f01c4a385dbdbe677440730236abbc327
Nando PL <nandopiless@gmail.com>
2023-11-22 11:28:16 +0100

    Ex 2 done

---
+++ 1T/Examen1T_Piles_Ferran/Ejercicio2/ejercicio2.js
@@ -0,0 +1,48 @@
+const imgFruitsPaths = [
+    "url(./images/frutas/1.jpg)",
+    "url(./images/frutas/2.jpg)",
+    "url(./images/frutas/3.jpg)",
+];
+
+const imgFlowersPaths = [
+    "url(./images/flores/1.jpg)",
+    "url(./images/flores/2.jpg)",
+    "url(./images/flores/3.jpg)"
+];
+
+
+
+const getRandomPhoto = (array) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+};
+
+/**
+ * Displays the images in the current order.
+ * @returns {any}
+ */
+const displayFruitsImgs = (interval) => {
+    imgFruitsPaths.forEach((_, index) => {
+        document.getElementsByClassName('photo')[index].style.backgroundImage = getRandomPhoto(imgFruitsPaths);
+        document.getElementsByClassName('photo')[index].addEventListener('click', () => clearInterval(interval));
+    });
+};
+
+/**
+ * Displays the images in the current order.
+ * @returns {any}
+ */
+const displayFlowersImgs = (interval) => {
+    imgFlowersPaths.forEach((_, index) => {
+        document.getElementsByClassName('photo')[index + 3].style.backgroundImage = getRandomPhoto(imgFlowersPaths);
+        document.getElementsByClassName('photo')[index + 3].addEventListener('click', () => clearInterval(interval));
+    });
+};
+
+
+const intervalFruits = setInterval(() => {
+    displayFruitsImgs(intervalFruits);
+}, 700);
+const intervalFlowers = setInterval(() => {
+    displayFlowersImgs(intervalFlowers);
+}, 700);
