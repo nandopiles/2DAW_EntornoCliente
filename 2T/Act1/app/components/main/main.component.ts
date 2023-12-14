e6b0ac0a0a319d1d8e02fdb3af8be9e0a1683302
Nando PL <nandopiless@gmail.com>
2023-12-14 13:11:18 +0100

    elements created Ej2

--- 2T/Act1/app/components/main/main.component.ts
+++ 2T/Act1/app/components/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ArticleComponent } from '../article/article.component';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ArticleComponent, MenuComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
