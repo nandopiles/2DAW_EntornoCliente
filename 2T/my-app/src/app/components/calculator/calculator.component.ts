003b45bb14faed1f7944429cae59bb81d54c92aa
Nando PL <nandopiless@gmail.com>
2023-12-29 11:48:41 +0100

    Footer component done

--- 2T/my-app/src/app/components/calculator/calculator.component.ts
+++ 2T/my-app/src/app/components/calculator/calculator.component.ts
@@ -8,7 +8,48 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
-  public getValue(num: number): void {
-    
+  public value: string = "0";
+  public isOperationOnCourse: boolean = false;
+  // public operationNumbers: number[] = [];
+
+
+
+  /**
+   * Gets the value of the num clicked.
+   * @param {number} num
+   * @returns {void}
+   */
+  public getNum(num: number): void {
+    const numString: string = num.toString();
+    (this.value === "0") ? this.value = numString : this.value += numString;
+  }
+
+  /**
+   * Does the "plus" operation with the nums selected.
+   * @returns {void}
+   */
+  public plus(): void {
+
+    let numbersToPlus: number[] = [];
+
+    // numbersToPlus = this.getValues(numbersToPlus);
+    console.log(numbersToPlus);
+
+  }
+
+  /**
+   * Resets the number wrote into 0 to write another number.
+   * @returns {void}
+   */
+  public resetNum(): void {
+    this.value = "0";
+  }
+
+  /**
+   * Deletes all the values and operations that were occurring.
+   * @returns {void}
+   */
+  public deleteAll(): void {
+    this.value = "0";
   }
 }
