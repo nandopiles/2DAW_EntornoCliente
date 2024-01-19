3bf7c6a42a85845559c8b6fffa68ba0571708683
Nando PL <nandopiless@gmail.com>
2024-01-19 11:33:41 +0100

    MemigoTwo function done

--- 2T/my-app/src/app/views/memigo-two/memigo-two.component.ts
+++ 2T/my-app/src/app/views/memigo-two/memigo-two.component.ts
@@ -12,8 +12,31 @@ import { RequiredService } from '../../services/required.service';
 })
 export class MemigoTwoComponent {
   public memes: Meme[] = [];
+  public currentIndex: number = 0;
 
   public constructor(public service: RequiredService) { }
 
-  
+  /**
+   * Goes to the next position of the carrousel.
+   * @returns {void}
+   */
+  public nextPosition(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.memes.length;
+  }
+
+  /**
+   * Goes to the previous position of the carrousel.
+   * @returns {void}
+   */
+  public prevPosition(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.memes.length) % this.memes.length;
+  }
+
+  /**
+   * Executes the function automatically.
+   * @returns {void}
+   */
+  public ngOnInit(): void {
+    this.memes = this.service.getMemes();
+  }
 }
